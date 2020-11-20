65639e780ce8526d8727e9a507bfcbdc66324e69
David Hepworth <davidhepp2@gmail.com>
2020-11-20 01:19:42 +0000

    Remove some dependencies from gen-config

--- builder/source/gen-config.ts
+++ builder/source/gen-config.ts
@@ -1,19 +1,19 @@
-import Path from "path";
 import Webpack from "webpack";
-import WebpackNodeExternals from "webpack-node-externals";
-import Yarn from "./yarn";
+import webpackNodeExternals from "webpack-node-externals";
 import webpackMerge from "webpack-merge";
 
-export default async function config(): Promise<Webpack.Configuration> {
-  const workspaceRoot = await Yarn.findWorkspaceRoot();
-  if (!workspaceRoot) throw new Error("Could not find workspace root");
+export interface EntryPoint {
+  readonly name: string;
+  readonly main: string;
+}
 
-  const workspaces = await Yarn.getWorkspaces();
-  const workspaceNames = Object.keys(workspaces);
-
-  const outputDir = Path.resolve(workspaceRoot, "dist");
-
-  const baseConfig: Webpack.Configuration = {
+export default function genConfig(
+  entryPoints: EntryPoint[],
+  outputDir: string,
+  modulesDirs: string[],
+  workspaceNames: string[]
+): Webpack.Configuration {
+  const baseConfig = {
     output: {
       path: outputDir,
       filename: "[name]/index.js",
@@ -30,26 +30,16 @@ export default async function config(): Promise<Webpack.Configuration> {
       ],
     },
     externals: [
-      WebpackNodeExternals({
-        additionalModuleDirs: [Path.resolve(workspaceRoot, "node_modules")],
+      webpackNodeExternals({
+        additionalModuleDirs: modulesDirs,
         allowlist: [...workspaceNames],
       }),
     ],
   };
 
-  const entries: Webpack.Configuration[] = [
-    {
-      entry: {
-        app: Path.resolve(
-          workspaceRoot,
-          "components",
-          "app",
-          "source",
-          "index.ts"
-        ),
-      },
-    },
-  ];
+  const entries: Webpack.Configuration[] = entryPoints.map((ep) => ({
+    entry: { [ep.name]: ep.main },
+  }));
 
-  return webpackMerge(baseConfig, ...entries);
+  return webpackMerge([baseConfig, ...entries]);
 }
--- builder/source/main.ts
+++ builder/source/main.ts
@@ -1,11 +1,42 @@
+import Path from "path";
 import Webpack from "webpack";
-import config from "./gen-config";
+import Yarn from "./yarn";
+import genConfig from "./gen-config";
 
 async function run() {
-  const compiler = Webpack(await config());
-  compiler.run((err, stats) => {
-    if (err) console.error(err);
-    if (stats) console.log(stats.toString({ colors: true }));
+  const workspaceRoot = await Yarn.findWorkspaceRoot();
+  if (!workspaceRoot) throw new Error(`Could not find workspace root`);
+
+  const outputDir = Path.resolve(workspaceRoot, "dist");
+  const modulesDirs = [Path.resolve(workspaceRoot, "node_modules")];
+
+  const workspaces = await Yarn.getWorkspaces();
+  const workspaceNames = Object.keys(workspaces);
+  const entryPoints = [
+    {
+      name: "app",
+      main: Path.resolve(
+        workspaceRoot,
+        "components",
+        "app",
+        "source",
+        "index.ts"
+      ),
+    },
+  ];
+
+  const config = genConfig(entryPoints, outputDir, modulesDirs, workspaceNames);
+
+  Webpack(config, (err, stats) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+
+    if (stats) {
+      console.log(stats.toString({ colors: true }));
+      return;
+    }
   });
 }
 
