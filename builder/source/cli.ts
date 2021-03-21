0eb9edbbfd8f82166402c5a38ceebcb561cf88f3
David Hepworth <davidhepp2@gmail.com>
2021-03-21 15:49:19 +0000

    Tidy up code

--- builder/source/cli.ts
+++ builder/source/cli.ts
@@ -1,11 +1,50 @@
 import * as Yarn from "@yarnpkg/core";
 import * as YarnFS from "@yarnpkg/fslib";
 
-import { ManifestGenerator } from "./config-generator";
+function unique<T>(value: T, index: number, array: T[]): boolean {
+  return array.indexOf(value) === index;
+}
 
 function prettyName(ws: Yarn.Workspace): string {
   if (!ws.manifest.name) return "[unnamed workspace]";
   return Yarn.structUtils.stringifyIdent(ws.manifest.name);
+}
+
+type DependencyMap = Map<Yarn.IdentHash, Yarn.Descriptor>;
+type DependencyPair = [Yarn.IdentHash, Yarn.Descriptor];
+
+// TODO: Handle circular dependencies between workspaces
+function collectDependencies(
+  project: Yarn.Project,
+  workspace: Yarn.Workspace
+): DependencyPair[] {
+  return Array.from(workspace.manifest.dependencies)
+    .map(([ident, desc]): DependencyPair[] => {
+      const ws = project.workspacesByIdent.get(ident);
+      return ws ? collectDependencies(project, ws) : [[ident, desc]];
+    })
+    .reduceRight((collected, deps) => collected.concat(deps), []);
+}
+
+// TODO: Check version conflicts
+function transitiveDependencies(
+  project: Yarn.Project,
+  workspace: Yarn.Workspace
+): DependencyMap {
+  return new Map(collectDependencies(project, workspace).filter(unique));
+}
+
+function makePackageManifest(
+  project: Yarn.Project,
+  workspace: Yarn.Workspace
+): Yarn.Manifest {
+  const manifest = new Yarn.Manifest();
+  manifest.name = workspace.manifest.name;
+  manifest.version = workspace.manifest.version;
+  manifest.main = YarnFS.npath.toPortablePath("index.js");
+  manifest.dependencies = transitiveDependencies(project, workspace);
+
+  return manifest;
 }
 
 async function run() {
@@ -15,14 +54,9 @@ async function run() {
   const yarnConfig = await Yarn.Configuration.find(cwd, null);
   const { project } = await Yarn.Project.find(yarnConfig, cwd);
 
-  const workspaces = project.workspaces.filter((ws) =>
-    toBuild.includes(prettyName(ws))
-  );
-
-  const manifestGenerator = new ManifestGenerator(project);
-  const manifests = workspaces.map((ws) =>
-    manifestGenerator.makePackageManifest(ws)
-  );
+  const manifests = project.workspaces
+    .filter((ws) => toBuild.includes(prettyName(ws)))
+    .map((ws) => makePackageManifest(project, ws));
 
   console.log(manifests.map((manifest) => manifest.exportTo({})));
 }
