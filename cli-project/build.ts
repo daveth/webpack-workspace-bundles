478f547edce4d3247a5cb1399ce926c3d8593819
David Hepworth <davidhepp2@gmail.com>
2021-03-21 22:37:32 +0000

    tidier import of fs.promises

--- cli-project/build.ts
+++ cli-project/build.ts
@@ -1,4 +1,4 @@
-import * as fs from "fs";
+import { promises as fs } from "fs";
 import * as esbuild from "esbuild";
 import * as daveth from "@daveth/builder";
 
@@ -22,10 +22,7 @@ async function build() {
     external: app.externals,
   });
 
-  await fs.promises.writeFile(
-    "dist/package.json",
-    JSON.stringify(app.manifest)
-  );
+  await fs.writeFile("dist/package.json", JSON.stringify(app.manifest));
 }
 
 build().catch(console.error);
